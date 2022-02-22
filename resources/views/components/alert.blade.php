cf99416e19ba7d4486f570de0a17ea7ca3bacac2
Felipe <felipe3841@gmail.com>
2022-02-22 11:39:00 -0500

    Generando rutas para livewire.js

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,6 +1,7 @@
 {{-- Así se hace uso de la variable que se recibe en la clase del componente --}}
-{{-- <div class="bg-{{ $color }}-100 border-l-4 border-{{ $color }}-500 text-{{ $color }}-700 p-4" role="alert"> --}}
-<div class="bg-blue-100 border-l-4 border-blue-500 text-blue-700 p-4" role="alert">
+<div class="bg-{{ $color }}-100 border-l-4 border-{{ $color }}-500 text-{{ $color }}-700 p-4" role="alert">
     <p class="font-bold">Be Warned</p>
     <p>Something not ideal might be happening.</p>
 </div>
+
+<header
